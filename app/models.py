6811eab2a246423ad1a10b16e38d1c557f2fd9aa
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-03-19 22:35:00 +0700

    xong bai 16

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 # Create your models here.
+
 class Customer(models.Model):
   user = models.OneToOneField(User,on_delete=models.SET_NULL,null = True, blank = False)
   name = models.CharField(max_length=200,null=True)
